0c8b1a7446e967865732636b478959a3190ca6f7
Can Guler <canmuhammetguler@gmail.com>
2020-12-02 14:30:52 -0600

    api fetched

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        Welcome!!
+        Welcome to the Meme factory!!!
       </div>
     )
   }
---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const RECEIVE_MEMES = "RECEIVE_MEMES";
+
+function receiveMemes(json){
+    const { memes } = json.data;
+
+    return {
+        type: RECEIVE_MEMES,
+        memes
+    }
+}
