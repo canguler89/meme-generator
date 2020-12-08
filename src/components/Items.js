0af27624316a52b221a44803d7f1a97dde4a634b
Can Guler <canmuhammetguler@gmail.com>
2020-12-07 22:11:17 -0600

    some more styling

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 
 class Items extends Component {
+    constructor(){
+        super();
+        this.state={
+            hovered:false
+        }
+    }
     render() {
         return (
-            <div className="meme-item">
+            <div className="meme-item"
+            onMouseEnter={()=>this.setState({hovered:true})}
+            onMouseLeave={()=>this.setState({hovered:false})}
+            >
                 <img src={this.props.meme.url}
                 alt={this.props.meme.name}
-                className="meme-img"
+                className={ this.state.hovered ? "meme-img darken-img" : "meme-img"}
                 />
-                <p className="meme-text">
+                <p className={ this.state.hovered ? "meme-text" : "no-text" }>
                     {this.props.meme.name}
                 </p>
             </div>
